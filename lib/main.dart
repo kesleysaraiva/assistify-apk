83c008165d591d87e706fe72d58da904f1385511
kesleysaraiva <kesleysaraiva@outlook.com.br>
2026-09-03 14:17:42 -0300

    Remove MediaKit initialization
    Removed MediaKit initialization from main function.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:media_kit/media_kit.dart';
 import 'theme/app_theme.dart';
 import 'services/xtream_service.dart';
 import 'services/storage_service.dart';
@@ -7,7 +6,6 @@ import 'screens/login_screen.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  MediaKit.ensureInitialized();
   runApp(const AssistifyApp());
 }
 
@@ -18,7 +16,6 @@ class AssistifyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     final xtream = XtreamService();
     final storage = StorageService();
-
     return MaterialApp(
       title: 'Assistify',
       debugShowCheckedModeBanner: false,
