ee60d826579a68775aa48b2afc0746f208fe8a2f
kesleysaraiva <kesleysaraiva@outlook.com.br>
2026-09-03 17:26:02 -0300

    Initialize Flutter application with main structure

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'theme/app_theme.dart';
+import 'services/xtream_service.dart';
+import 'services/storage_service.dart';
+import 'screens/login_screen.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const AssistifyApp());
+}
+
+class AssistifyApp extends StatelessWidget {
+  const AssistifyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final xtream = XtreamService();
+    final storage = StorageService();
+    return MaterialApp(
+      title: 'Assistify',
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme.dark,
+      home: LoginScreen(xtream: xtream, storage: storage),
+    );
+  }
+}
