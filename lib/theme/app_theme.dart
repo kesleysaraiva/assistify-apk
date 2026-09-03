25aa170c6ba16c8000f990aeb833767604e55e68
kesleysaraiva <kesleysaraiva@outlook.com.br>
2026-09-03 17:57:42 -0300

    Update cardTheme to use CardThemeData

--- lib/theme/app_theme.dart
+++ lib/theme/app_theme.dart
@@ -66,7 +66,7 @@ class AppTheme {
           elevation: 0,
         ),
       ),
-      cardTheme: CardTheme(
+      cardTheme: CardThemeData(
         color: AppColors.bgSecondary,
         elevation: 0,
         shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
